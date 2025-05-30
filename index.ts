dd4a4128df5ee4aa3c8264d91b60c7a2069ec04f
Shema-Philbert-Gasana <shemaphilbert8@gmail.com>
2025-05-30 10:20:37 +0200

    created properties array

--- index.ts
+++ index.ts
@@ -2,9 +2,8 @@
 // Write a function that will display the most recent reviewers name next to the review total,
 // making sure to assign a type to the parameter, to prevent unwanted behaviour.
 
-const returningUserDisplay = document.querySelector("#returning-user");
-const userNameDisplay = document.querySelector("#user");
-const reviewTotalDisplay = document.querySelector("#reviews");
+import {showReviewTotal,populateUser} from './utils.ts'
+let isOpen: boolean;
 
 const reviews: {
   name: string;
@@ -32,17 +31,7 @@ const reviews: {
   },
 ];
 
-// Solution
-function showReviewTotal(value: number, name: string, star: boolean) {
-  reviewTotalDisplay.innerHTML =
-    "review total " +
-    value.toString() +
-    " | last review was from " +
-    name +
-    ` ${star ? "⭐" : ""}`;
-}
-
-showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser);
+// User
 
 const you: {
   firstName: string;
@@ -58,11 +47,83 @@ const you: {
   stayedAt: ["florida-home", "oman-flat", "tokyo-bungalow", 23],
 };
 
-function populateUser(isReturning: boolean, userName: string) {
-  if (isReturning) {
-    returningUserDisplay.innerHTML = "back";
-  }
-  userNameDisplay.innerHTML = userName;
-}
+// property
+// const properties: {
+//     image:string;
+//     title:string;
+//     pricePerNight:number;
+//     address:string;
+//     city:string;
+//     zipCode:string;
+//     country:string;
+//     contacts:string;
+//     availableForRent:boolean;
+// }[] = [{
+//     image:"logo.png",
+//     title:"Couch",
+//     pricePerNight:15,
+//     address:'kigali',
+//     city:'kicukiro',
+//     zipCode:'0000',
+//     country:'kigali',
+//     contacts:'078888888',
+//     availableForRent:false,
+// }]
 
-populateUser(you.isReturning, you.userName);
+const properties : {
+    image: string;
+    title: string;
+    price: number;
+    location: {
+        firstLine: string;
+        city: string;
+        code: number;
+        country: string;
+    };
+    contact: string;
+    isAvailable: boolean;
+}[] = [
+    {
+        image: 'images/colombia-property.jpg',
+        title: 'Colombian Shack',
+        price: 45,
+        location: {
+            firstLine: 'shack 37',
+            city: 'Bogota',
+            code: 45632,
+            country: 'Colombia'
+        },
+        contact: 'marywinkle@gmail.com',
+        isAvailable: true  
+    },
+    {
+        image: 'images/poland-property.jpg',
+        title: 'Polish Cottage',
+        price: 34,
+        location: {
+            firstLine: 'no 23',
+            city: 'Gdansk',
+            code: 343903,
+            country: 'Poland'
+        },
+        contact: 'garydavis@hotmail.com',
+        isAvailable: false 
+    },
+    {
+        image: 'images/london-property.jpg',
+        title: 'London Flat',
+        price: 23,
+        location: {
+            firstLine: 'flat 15',
+            city: 'London',
+            code: 35433,
+            country: 'United Kingdom',
+        },
+        contact: 'andyluger@aol.com',
+        isAvailable: true
+    }
+]
+
+showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser)
+
+populateUser(you.isReturning, you.firstName)
