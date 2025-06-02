7dd6f83eb6e92f5833408b9bdc048e9a36659d16
Shema-Philbert-Gasana <shemaphilbert8@gmail.com>
2025-06-02 13:18:27 +0200

    function types

--- utils.ts
+++ utils.ts
@@ -1,40 +1,30 @@
-import { LoyaltyUser } from "./enums"
-
 const reviewTotalDisplay = document.querySelector('#reviews')
 const returningUserDisplay = document.querySelector('#returning-user')
 const userNameDisplay = document.querySelector('#user')
+import { LoyaltyUser, Permissions } from './enums'
 
-export function showReviewTotal(value: number, reviewer: string, isLoyalty: LoyaltyUser) :void{
-    const iconDisplay = isLoyalty === 'Gold' ? '⭐' : ''
-    reviewTotalDisplay.innerHTML = 'review total ' + value.toString() + '| last reviewed by ' + reviewer + ' ' + iconDisplay
+export function showReviewTotal(value: number, reviewer: string, isLoyalty: LoyaltyUser) {
+    const iconDisplay = LoyaltyUser.GOLD_USER ? '⭐' : ''
+    reviewTotalDisplay.innerHTML = value.toString()+' review' + makeMultiple(value) + ' | last reviewed by ' + reviewer + ' ' + iconDisplay
 }
 
-export function populateUser(isReturning : boolean, userName: string ):void {
-    if (isReturning == true){
+export function populateUser(isReturning : boolean, userName: string ) {
+    if (isReturning){
         returningUserDisplay.innerHTML = 'back'
     }
     userNameDisplay.innerHTML = userName
 }
-function add( firstValue: number, secondValue: number ) : number {
-    return firstValue + secondValue
-}
 
-//explanatory function for union types
-function add(firstValue : (number | string), secondValue: (number | string)) {
-    let result
-    if (typeof firstValue === 'number' && typeof secondValue === 'number') {
-        result = firstValue + secondValue
-    }
-    if (typeof firstValue === 'string' && typeof secondValue === 'string') {
-        result = firstValue + ' ' + secondValue
-    }
-    if (typeof firstValue === 'number' && typeof secondValue === 'string') {
-        console.log('cannot perform this addition')
-    }
-    if (typeof firstValue === 'string' && typeof secondValue === 'number') {
-        console.log('cannot perform this addition')
+export function showDetails(value: boolean | Permissions, element : HTMLDivElement, price: number) {
+    if (value) {
+        const priceDisplay = document.createElement('div')
+        priceDisplay.innerHTML = price.toString() + '/night'
+        element.appendChild(priceDisplay)
     }
 }
 
-const combinedReviews = add(5,1)
-const firstNameLastName = add('Ania', 'Kubow')
+export function makeMultiple(value: number):string {
+    if (value > 1 || value == 0 ) {
+        return 's'
+    } else return ''
+}
