b3c76a73068b60109575ae9d2d7cdeed366d9805
Shema-Philbert-Gasana <shemaphilbert8@gmail.com>
2025-06-02 13:08:58 +0200

    adding a type to a function

--- utils.ts
+++ utils.ts
@@ -4,18 +4,22 @@ const reviewTotalDisplay = document.querySelector('#reviews')
 const returningUserDisplay = document.querySelector('#returning-user')
 const userNameDisplay = document.querySelector('#user')
 
-export function showReviewTotal(value: number, reviewer: string, isLoyalty: LoyaltyUser) {
+export function showReviewTotal(value: number, reviewer: string, isLoyalty: LoyaltyUser) :void{
     const iconDisplay = isLoyalty === 'Gold' ? '⭐' : ''
     reviewTotalDisplay.innerHTML = 'review total ' + value.toString() + '| last reviewed by ' + reviewer + ' ' + iconDisplay
 }
 
-export function populateUser(isReturning : boolean, userName: string ) {
+export function populateUser(isReturning : boolean, userName: string ):void {
     if (isReturning == true){
         returningUserDisplay.innerHTML = 'back'
     }
     userNameDisplay.innerHTML = userName
 }
-//explanatory function
+function add( firstValue: number, secondValue: number ) : number {
+    return firstValue + secondValue
+}
+
+//explanatory function for union types
 function add(firstValue : (number | string), secondValue: (number | string)) {
     let result
     if (typeof firstValue === 'number' && typeof secondValue === 'number') {
